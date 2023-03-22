7bf687c42b411bbba9ffb4ea4565fbdda74d476b
Deontae Carter <deontaecarter93@gmail.com>
2023-03-22 22:56:35 +0000

    Code8

---
+++ code8.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+# # How to create and print a list
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist)
+
+# # How to print the first item only
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist[0])
+
+# # How to print the last item only by counting backwards
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist[-1])
+
+# # How to print a range of elements in a list
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[2:5])
+
+# # How to print the beginning to a specific element number
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[:4])
+
+# # How to add an item to a list
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.append("orange")
+# print(thislist)
+
+#!/usr/bin/env python3
+
+#Assign to a variable a list of ten string elements.
+my_list = [ "green" , "blue" , "red" , "yellow" , "orange" , "purple" , "gray" , "black" , "pink" , "white"  ]
+print(my_list)
+
+# #Print the fourth element of the list.
+print(my_list[3])
+
+#Print the sixth through tenth element of the list.
+print(my_list[5:10])
+
+#Change the value of the seventh element to “onion”.
+my_list[6] = "onions"
+print(my_list)
