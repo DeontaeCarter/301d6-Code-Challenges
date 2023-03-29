4201c88d0b06a0eb9ae500aeb7f04651443ec14b
Deontae Carter <deontaecarter93@gmail.com>
2023-03-29 15:44:32 +0000

    Code Challenge 10

--- code07.py
+++ code07.py
@@ -2,23 +2,23 @@
 
 # Import libraries
 
-import os
+# import os
 
-# Declaration of variables
+# # Declaration of variables
 
-### Read user input here into a variable
+# ### Read user input here into a variable
 
-# Declaration of functions
+# # Declaration of functions
 
-### Declare a function here
+# ### Declare a function here
 
-for (root, dirs, files) in os.walk("testdir"):
-    ### Add a print command here to print ==root==
-    print(root)
-    ### Add a print command here to print ==dirs==
-    print(dirs)
-    ### Add a print command here to print ==files==
-    print(files)
+# for (root, dirs, files) in os.walk("testdir"):
+#     ### Add a print command here to print ==root==
+#     print(root)
+#     ### Add a print command here to print ==dirs==
+#     print(dirs)
+#     ### Add a print command here to print ==files==
+#     print(files)
 
 # Main
 
