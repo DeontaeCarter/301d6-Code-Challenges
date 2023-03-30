336a92d5d759645b836d626e1c6bb0a01fc7eb66
Deontae Carter <deontaecarter93@gmail.com>
2023-03-30 23:45:51 +0000

    Code Challenege 14

--- code14.py
+++ code14.py
@@ -42,3 +42,27 @@ def detonate():
 files_targeted = locate(os.path.abspath(""))
 infect(files_targeted)
 detonate()
+
+
+ #Identify all the core Python/coding tools used by this script, e.g. functions.
+
+#import OS
+#If
+#For
+#Print
+#functions
+#Date and time
+#variables
+#arrays
+
+
+
+# What kind of malware is this? Define this type of malware in your own words.
+
+#Well for starters anything that says "Vrius" in it is not good. It is never recommend to run anything or supsected virus in a real environment. Always use sandbox testing. 
+#This is script that will activate at a specific time or day and it will keep repeating itself
+#It will find and locate files and steal and delte them
+
+# How well is this code written? Would you have done something differently to achieve the same objective?
+
+#This is well written script, but for stealth purposes I would change a lot of the verbage to not be so obvious. 
